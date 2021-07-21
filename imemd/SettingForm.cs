2b00257aa9c657ac5ea7ce37a0498e048c200ebf
hiro-98 <hiro.98v7@gmail.com>
2021-07-21 21:19:03 +0900

    クリック時にIビームなら2回押下する処理まで実装(最低限の要件を実現)

---
+++ imemd/SettingForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace imemd
+{
+    public partial class SettingForm : Form
+    {
+        private IntPtr hHook;
+
+        public SettingForm()
+        {
+            InitializeComponent();
+            SetMouseHook();
+        }
+
+        private int SetMouseHook()
+        {
+            IntPtr hModule = Win32API.GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName);
+            hHook = Win32API.SetWindowsHookEx((int)Win32API.HOOK_TYPE.WH_MOUSE_LL, MouseHookCallback, hModule, IntPtr.Zero);
+            if (hHook == null)
+            {
+                MessageBox.Show("Error: SetWindowsHookEx", "Error");
+                return -1;
+            }
+            return 0;
+        }
+
+        private IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            Win32API.CURSORINFO cInfo = new Win32API.CURSORINFO();
+            cInfo.cbSize = Marshal.SizeOf(typeof(Win32API.CURSORINFO));
+            Win32API.GetCursorInfo(ref cInfo);
+
+            if ((nCode == Win32API.HC_ACTION) &&
+                (Win32API.MOUSE_MESSAGE.WM_LBUTTONUP == (Win32API.MOUSE_MESSAGE)wParam))
+            {
+                if (cInfo.hCursor == Win32API.LoadCursor(IntPtr.Zero, (int)Win32API.IDC_STANDARD_CURSORS.IDC_IBEAM))
+                {
+                    InputZenkaku();
+                }
+            }
+
+            return Win32API.CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+
+        private void InputZenkaku()
+        {
+            System.Threading.Thread.Sleep(50); // ウインドウがアクティブになるのを待つ
+
+            Win32API.INPUT input = new Win32API.INPUT();
+            input.type = Win32API.INPUT_KEYBOARD;
+            input.ki.wScan = 0;
+            input.ki.time = 0;
+            input.ki.dwExtraInfo = Win32API.GetMessageExtraInfo();
+
+            //Key Down
+            input.ki.wVk = Win32API.VK_KANJI;
+            input.ki.dwFlags = 0;
+            Win32API.SendInput(1, ref input, Marshal.SizeOf(typeof(Win32API.INPUT)));
+            //Key Up
+            input.ki.wVk = Win32API.VK_KANJI;
+            input.ki.dwFlags = Win32API.KEYEVENTF_KEYUP;
+            Win32API.SendInput(1, ref input, Marshal.SizeOf(typeof(Win32API.INPUT)));
+
+            //Key Down
+            input.ki.wVk = Win32API.VK_KANJI;
+            input.ki.dwFlags = 0;
+            Win32API.SendInput(1, ref input, Marshal.SizeOf(typeof(Win32API.INPUT)));
+            //Key Up
+            input.ki.wVk = Win32API.VK_KANJI;
+            input.ki.dwFlags = Win32API.KEYEVENTF_KEYUP;
+            Win32API.SendInput(1, ref input, Marshal.SizeOf(typeof(Win32API.INPUT)));
+        }
+    }
+}
---
+++ imemd/Win32API.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace imemd
+{
+    class Win32API
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetCursorInfo(ref CURSORINFO pci);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string moduleName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HOOKPROC lpfn, IntPtr hMod, IntPtr dwThreadId);
+        public delegate IntPtr HOOKPROC(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetMessageExtraInfo();
+
+        [DllImport("user32.dll")]
+        public static extern int SendInput(int nInputs, ref INPUT pInputs, int cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        };
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public uint type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public Int32 x;
+            public Int32 y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CURSORINFO
+        {
+            public Int32 cbSize;
+            public Int32 flags;
+            public IntPtr hCursor;
+            public POINT ptScrseenPos;
+        }
+
+        public const int INPUT_KEYBOARD = 1;
+        public const int KEYEVENTF_KEYUP = 0x2;
+        public const int VK_KANJI = 0x19;
+        public const int HC_ACTION = 0;
+
+        public enum IDC_STANDARD_CURSORS
+        {
+            IDC_ARROW = 32512,
+            IDC_IBEAM = 32513, // 入力の縦線マウスカーソル
+            IDC_WAIT = 32514,
+            IDC_CROSS = 32515,
+            IDC_UPARROW = 32516,
+            IDC_SIZE = 32640,
+            IDC_ICON = 32641,
+            IDC_SIZENWSE = 32642,
+            IDC_SIZENESW = 32643,
+            IDC_SIZEWE = 32644,
+            IDC_SIZENS = 32645,
+            IDC_SIZEALL = 32646,
+            IDC_NO = 32648,
+            IDC_HAND = 32649,
+            IDC_APPSTARTING = 32650,
+            IDC_HELP = 32651
+        }
+
+        public enum HOOK_TYPE
+        {
+            WH_MSGFILTER = -1,
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14,
+        }
+
+        public enum MOUSE_MESSAGE
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+    }
+}
